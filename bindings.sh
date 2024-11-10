2e9865a372b55d897341474fa77a05e8da5a2757
kodincc <marriagadar@icomercial.ucsc.cl>
2024-11-10 12:40:58 -0300

    Update bindings.sh

--- bindings.sh
+++ bindings.sh
@@ -9,7 +9,7 @@ while IFS= read -r line || [ -n "$line" ]; do
     value=$(echo $value | sed 's/^"\(.*\)"$/\1/')
     bindings+="--binding ${name}=${value} "
   fi
-done < .env
+done < .env.local
 
 bindings=$(echo $bindings | sed 's/[[:space:]]*$//')
 
